9380d367b3cc903ade865180f37f269d61abf318
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:38:09 -0500

    Add files via upload

---
+++ Test-Lists-For-In.py
@@ -0,0 +1,23 @@
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+
+# student['phone'] = '555-1234'
+# student['name'] = 'Jane'
+# student.update({'name': 'Jane', 'age': 26, 'phone':'555-1234'})
+
+# del student ['age']
+# age = student.pop('age')
+# print(age)
+
+# print(student.get['name'])
+# print(student.get('phone', 'Not Found'))
+# print(student)
+
+# print(len(student))
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+for key, value in student.items():
+    print(key + ":", value)
+
